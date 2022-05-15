e4748d386d921c2542738ed18f94b89718e0e944
Gabriel Freitas <freitasga.01@gmail.com>
2022-05-15 19:07:42 -0300

    feat: add-login-with-temporary-password

--- src/components/RedirectRoute.jsx
+++ src/components/RedirectRoute.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
-import { Center, Loader } from '@mantine/core';
 
 import { useAppContext } from '../context/appContext';
 
 // eslint-disable-next-line react/prop-types
 export const RedirectRoute = ({ children }) => {
   const location = useLocation();
-  const { currentUser, loading } = useAppContext();
-
-  if (loading) {
-    return (
-      <Center sx={{ height: '100vh' }}>
-        <Loader color="green" />
-      </Center>
-    );
-  }
+  const { currentUser } = useAppContext();
 
   return !currentUser ? children : <Navigate to="/projects" state={{ from: location }} replace />;
 };
